7a194465cfc491aad5bcba1ab0054cd42b059c35
Wendell <wendell@Wendells-Mac-mini.local>
2019-09-12 09:47:04 +0800

    access Context using Consumer in functional component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
-  static contextType = ThemeContext;
+function BookList() {
+  return (
+    <ThemeContext.Consumer>
+      {context => {
+        const { isLightTheme, light, dark } = context;
+        const theme = isLightTheme ? light : dark;
 
-  render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-
-    return (
-      <div
-        className='book-list'
-        style={{ color: theme.syntax, background: theme.bg }}
-      >
-        <ul>
-          <li style={{ background: theme.ui }}>the way of kings</li>
-          <li style={{ background: theme.ui }}>the name of the wind</li>
-          <li style={{ background: theme.ui }}>the final empire</li>
-        </ul>
-      </div>
-    );
-  }
+        return (
+          <div
+            className='book-list'
+            style={{ color: theme.syntax, background: theme.bg }}
+          >
+            <ul>
+              <li style={{ background: theme.ui }}>the way of kings</li>
+              <li style={{ background: theme.ui }}>the name of the wind</li>
+              <li style={{ background: theme.ui }}>the final empire</li>
+            </ul>
+          </div>
+        );
+      }}
+    </ThemeContext.Consumer>
+  );
 }
 
 export default BookList;
